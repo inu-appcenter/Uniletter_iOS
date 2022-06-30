1ec5bcd3722aec17996dcf723579736dc81236ab
leemhyungyu <lhglbkksj@naver.com>
2022-06-30 20:45:09 +0900

    [ADD] MyPageViewModel 파일 추가 및 전반적인 MyPageVC UI구현

--- Uniletter/Sources/Controllers/MyPageViewController.swift
+++ Uniletter/Sources/Controllers/MyPageViewController.swift
@@ -15,7 +15,14 @@ class MyPageViewController: UIViewController {
         let scrollView = UIScrollView()
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         
+//        scrollView.backgroundColor = .systemGray4
         return scrollView
+    }()
+    
+    let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+//        tableView.isScrollEnabled = false
+        return tableView
     }()
     
     let infoView: UIView = {
@@ -65,6 +72,44 @@ class MyPageViewController: UIViewController {
         return btn
     }()
     
+    let saveListBtn: UIButton = {
+        
+        var config = UIButton.Configuration.plain()
+        config.baseBackgroundColor = .clear
+        config.image = UIImage(systemName: "bookmark.fill")
+        config.imagePadding = 10
+        config.imagePlacement = .leading
+        
+        let button = UIButton(configuration: config)
+        button.layer.cornerRadius = 8
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.setTitleColor(.black, for: .normal)
+        button.setTitle("저장목록", for: .normal)
+        
+        return button
+    }()
+    
+    let alarmListBtn: UIButton = {
+        
+        var config = UIButton.Configuration.plain()
+        config.baseBackgroundColor = .clear
+        config.image = UIImage(systemName: "bell.fill")
+        config.imagePadding = 10
+        config.imagePlacement = .leading
+        
+        let button = UIButton(configuration: config)
+        button.layer.cornerRadius = 8
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.setTitleColor(.black, for: .normal)
+        button.setTitle("알림목록", for: .normal)
+        
+        return button
+    }()
+    
+    var myPageViewModel = MyPageViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -76,47 +121,80 @@ class MyPageViewController: UIViewController {
     func configureUI() {
         
         view.addSubview(scrollView)
+
         scrollView.addSubview(infoView)
         
+        infoView.addSubview(tableView)
         infoView.addSubview(userImage)
         infoView.addSubview(userName)
         infoView.addSubview(grayBar)
         infoView.addSubview(changeBtn)
+        infoView.addSubview(saveListBtn)
+        infoView.addSubview(alarmListBtn)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(MyPageCell.self, forCellReuseIdentifier: MyPageCell.identifier)
         
         scrollView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(10)
-            $0.leading.equalTo(view).offset(10)
-            $0.width.equalTo(350)
-            $0.height.equalTo(1500)
+//            $0.top.bottom.equalTo(view.safeAreaLayoutGuide)
+//            $0.leading.trailing.equalTo(view)
+//            $0.width.equalTo(350)
+//            $0.height.equalTo(1000)
+            
+            $0.edges.equalToSuperview()
         }
-        
+
         infoView.snp.makeConstraints {
-            $0.edges.equalTo(scrollView.contentLayoutGuide)
-            $0.height.equalTo(300)
-            $0.width.equalTo(scrollView.snp.width)
+            $0.top.bottom.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalTo(view)
+//
+//            $0.edges.equalTo(scrollView.contentLayoutGuide)
+//            $0.height.greaterThanOrEqualTo(view.snp.height).priority(.low)
+//            $0.width.equalTo(scrollView.snp.width)
+
         }
-        
+
         userImage.snp.makeConstraints {
-            $0.top.equalTo(scrollView).inset(36)
-            $0.centerX.equalTo(scrollView)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(36)
+            $0.centerX.equalTo(view)
             $0.width.height.equalTo(78)
         }
-        
+
         userName.snp.makeConstraints {
             $0.top.equalTo(userImage.snp.bottom).offset(24)
-            $0.centerX.equalTo(scrollView)
+            $0.centerX.equalTo(view)
         }
-        
+
         grayBar.snp.makeConstraints {
             $0.top.equalTo(userName.snp.bottom).offset(4)
-            $0.centerX.equalTo(scrollView)
+            $0.centerX.equalTo(view)
             $0.width.equalTo(244)
             $0.height.equalTo(1)
         }
-        
+
         changeBtn.snp.makeConstraints {
             $0.top.equalTo(grayBar.snp.bottom).offset(4)
-            $0.centerX.equalTo(scrollView)
+            $0.centerX.equalTo(view)
+        }
+        
+        saveListBtn.snp.makeConstraints {
+            $0.top.equalTo(changeBtn.snp.bottom).offset(16)
+            $0.leading.equalToSuperview().inset(26)
+            $0.width.equalTo(160)
+            $0.height.equalTo(49)
+        }
+        
+        alarmListBtn.snp.makeConstraints {
+            $0.top.equalTo(changeBtn.snp.bottom).offset(16)
+            $0.leading.equalTo(saveListBtn.snp.trailing)
+            $0.width.equalTo(160)
+            $0.height.equalTo(49)
+        }
+        
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(saveListBtn.snp.bottom).offset(5)
+            $0.leading.trailing.bottom.equalTo(infoView)
         }
     }
     
@@ -138,5 +216,72 @@ class MyPageViewController: UIViewController {
     
     @objc func changeBtnClicked(_ sender: UIGestureRecognizer) {
         print("changeBtnClicked() - called")
+    }
+}
+
+extension MyPageViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return myPageViewModel.numOfSection
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        switch section {
+        case 0:
+            return myPageViewModel.titleOfSection(at: section)
+        case 1:
+            return myPageViewModel.titleOfSection(at: section)
+        case 2:
+            return myPageViewModel.titleOfSection(at: section)
+        case 3:
+            return myPageViewModel.titleOfSection(at: section)
+        default:
+            return "error"
+        }
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        50
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        switch section {
+        case 0:
+            return myPageViewModel.numOfCell(at: 0)
+        case 1:
+            return myPageViewModel.numOfCell(at: 1)
+        case 2:
+            return myPageViewModel.numOfCell(at: 2)
+        case 3:
+            return myPageViewModel.numOfCell(at: 3)
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MyPageCell.identifier, for: indexPath) as? MyPageCell else { return UITableViewCell() }
+        
+        switch indexPath.section {
+        case 0:
+            let text = myPageViewModel.type[indexPath.section].cell
+            cell.updateUI(at: text[indexPath.row])
+            return cell
+        case 1:
+            let text = myPageViewModel.type[indexPath.section].cell
+            cell.updateUI(at: text[indexPath.row])
+            return cell
+        case 2:
+            let text = myPageViewModel.type[indexPath.section].cell
+            cell.updateUI(at: text[indexPath.row])
+            return cell
+        case 3:
+            let text = myPageViewModel.type[indexPath.section].cell
+            cell.updateUI(at: text[indexPath.row])
+            return cell
+        default:
+            return UITableViewCell()
+        }
     }
 }
---
+++ Uniletter/Sources/MyPage/MyPageCell.swift
@@ -0,0 +1,42 @@
+//
+//  MyPageCell.swift
+//  Uniletter
+//
+//  Created by 임현규 on 2022/06/30.
+//
+
+import UIKit
+import SnapKit
+
+class MyPageCell: UITableViewCell {
+    
+    static let identifier = "MyPageCell"
+    
+    lazy var label: UILabel = {
+        var label = UILabel()
+        
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setting()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("")
+    }
+    
+    func setting() {
+        addSubview(label)
+        
+        label.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(20)
+            $0.centerY.equalToSuperview()
+        }
+    }
+    
+    func updateUI(at text: String) {
+        self.label.text = text
+    }
+}
