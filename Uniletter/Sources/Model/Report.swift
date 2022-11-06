cd4cfcb804290260d9d22be18a4320f643fd5abd
oh_jun <vhzkclq0705@naver.com>
2022-11-07 01:11:28 +0900

    [MOD] 파일 위치 수정

---
+++ Uniletter/Sources/Model/Report.swift
@@ -0,0 +1,13 @@
+//
+//  Report.swift
+//  Uniletter
+//
+//  Created by 임현규 on 2022/11/06.
+//
+import Foundation
+
+struct Report: Codable {
+    let userId: Int?
+    let eventId: Int?
+    let msg: String?
+}
