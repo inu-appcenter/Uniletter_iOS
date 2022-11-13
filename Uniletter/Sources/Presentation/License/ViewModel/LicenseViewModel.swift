b7821f86c890a55b17e3df8f27af704dfb555343
leemhyungyu <lhglbkksj@naver.com>
2022-11-13 23:09:23 +0900

    [ADD] Toast_Swift 오픈소스 라이센스 추가

--- Uniletter/Sources/Presentation/License/ViewModel/LicenseViewModel.swift
+++ Uniletter/Sources/Presentation/License/ViewModel/LicenseViewModel.swift
@@ -18,6 +18,7 @@ enum License: CaseIterable {
     case FSCalender
     case SwiftMessages
     case Firebase
+    case ToastSwift
     
     var title: String {
         switch self {
@@ -39,6 +40,8 @@ enum License: CaseIterable {
             return "SwiftMessages"
         case .Firebase:
             return "Firebase/Messaging"
+        case .ToastSwift:
+            return "Toast_Swift"
         }
     }
     
@@ -594,13 +597,36 @@ enum License: CaseIterable {
                See the License for the specific language governing permissions and
                limitations under the License.
             """
+        case .ToastSwift:
+            return """
+            Copyright (c) 2015-2017 Charles Scalesse.
+
+            Permission is hereby granted, free of charge, to any person obtaining a
+            copy of this software and associated documentation files (the
+            "Software"), to deal in the Software without restriction, including
+            without limitation the rights to use, copy, modify, merge, publish,
+            distribute, sublicense, and/or sell copies of the Software, and to
+            permit persons to whom the Software is furnished to do so, subject to
+            the following conditions:
+
+            The above copyright notice and this permission notice shall be included
+            in all copies or substantial portions of the Software.
+
+            THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+            OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+            MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+            IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+            CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+            TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+            SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+            """
         }
     }
 }
 
 class LicenseViewModel {
     
-    let Licenses: [License] = [.SnapKit, .Alamofire, .Kingfisher, .Marqueelabel, .GoogleSignln, .DropDown, .FSCalender, .SwiftMessages, .Firebase]
+    let Licenses: [License] = [.SnapKit, .Alamofire, .Kingfisher, .Marqueelabel, .GoogleSignln, .DropDown, .FSCalender, .SwiftMessages, .Firebase, .ToastSwift]
     
     var numOfCell: Int {
         return Licenses.count
